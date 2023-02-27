9af79a93f69f1febd77e534e97205429c559bc6a
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2023-02-27 16:22:06 +0100

    day1

---
+++ day01a/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace day01a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText(@"textd1.txt");
+
+            int max = 0;
+
+            foreach (var row in input.Split("\n\r"))
+            {
+                int groupsum = 0;
+
+                foreach (var calorie in row.Split("\r"))
+                {
+                    Int32.TryParse(calorie, out int lol);
+                    groupsum += lol;
+                }
+
+                if (groupsum > max)
+                {
+                    max = groupsum;
+
+                    Console.WriteLine(max);
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ day01b/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace day01b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText(@"textd1.txt");
+
+            int first = 0;
+            int second = 0;
+            int third = 0;
+
+            foreach (var row in input.Split("\n\r"))
+            {
+                int groupsum = 0;
+
+                foreach (var calorie in row.Split("\r"))
+                {
+                    Int32.TryParse(calorie, out int lol);
+                    groupsum += lol;
+                }
+
+                if (groupsum > first)
+                {
+                    third = second;
+                    second = first;
+                    first = groupsum;
+                }
+                else if (groupsum > second)
+                {
+                    third = second;
+                    second = groupsum;
+                }
+                else if (groupsum > third)
+                {
+                    third = groupsum;
+                }
+            }
+            int total = first + second + third;
+            Console.WriteLine($"first: {first}\nsecond: {second}\nthird: {third}\ntotal: {total}");
+        }
+    }
+}
